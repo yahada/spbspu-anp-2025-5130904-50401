209fb2044578a52a251a9167f297d5cb8cc59c0b
yahada <niadzy14@gmail.com>
2025-12-17 22:56:29 +0300

    Shape class and Rect class added

--- malashenko.dmitrii/P5/main.cpp
+++ malashenko.dmitrii/P5/main.cpp
@@ -1,4 +1,60 @@
 #include <iostream>
+
+
+namespace malashenko {
+
+  struct point_t {
+    double x, y;
+  };
+
+  struct rectangle_t {
+    double width, height;
+    point_t pos;
+  };
+
+  struct Shape {
+    virtual double getArea() const = 0;
+    virtual rectangle_t getFrameRect() const = 0;
+    virtual void move(point_t p) = 0;
+    virtual void move(double dx, double dy) = 0;
+    virtual void scale(double k) = 0;
+    virtual ~Shape() = default;
+  };
+  
+  struct Rectangle : Shape {
+    Rectangle(double width, double height, point_t pos);
+    double getArea() const override;
+    rectangle_t getFrameRect() const override;
+    void move(point_t p) override;
+    void move(double dx, double dy) override;
+    void scale(double k) override;
+
+    private:
+    double width_, height_;
+    point_t pos_;
+  };
+
+  Rectangle::Rectangle(double width, double height, point_t pos):
+    width_(width),
+    height_(height),
+    pos_(pos)
+  {
+
+  }
+
+  double Rectangle::getArea() const
+  {
+    return width_ * height_;
+  }
+
+  rectangle_t Rectangle::getFrameRect() const
+  {
+    rectangle_t frame = {
+
+    };
+  }
+  
+}
 int main()
 {
   std::cout << "Initial commit\n";
