34163135645a4f96c604e7c1ff935060bd35319e
yahada <niadzy14@gmail.com>
2026-01-22 11:40:50 +0300

    Remove old files after renaming

--- malashenko.dmitrii/P5/generalFuncs.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef GENERALFUNCS
-#define GENERALFUNCS
-#include "pod.hpp"
-#include <iostream>
-namespace malashenko {
-  rectangle_t generalGetFrameRect(const point_t * tops, size_t len);
-  double generalGetArea(const point_t * tops, size_t len);
-}
-#endif
